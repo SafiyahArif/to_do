aad4e0d5fbd2fae5258bb7bc603b9c348a78c293
SafiyahArif <arsafiyah@gmail.com>
2026-05-29 14:29:53 +0530

    4th commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,24 +12,113 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
 
   newTask = '';
-  tasks: { title: string; completed: boolean }[] = [];
+
+  selectedPriority = 'Medium';
+
+  tasks: {
+    title: string;
+    completed: boolean;
+    priority: string;
+    editing: boolean;
+  }[] = [];
+
+  editIndex: number | null = null;
+
+  isEditing = false;
+
+  constructor() {
+
+    const savedTasks = localStorage.getItem('tasks');
+
+    if (savedTasks) {
+
+      this.tasks = JSON.parse(savedTasks);
+
+    }
+  }
 
   addTodo() {
 
-    if (this.newTask.trim() !== '') {
+    if (this.newTask.trim() === '') return;
+
+    if (this.isEditing && this.editIndex !== null) {
+
+      this.tasks[this.editIndex].title =
+        this.newTask;
+
+      this.tasks[this.editIndex].priority =
+        this.selectedPriority;
+
+      this.isEditing = false;
+
+      this.editIndex = null;
+
+    } else {
 
       this.tasks.push({
+
         title: this.newTask,
-        completed: false
+
+        completed: false,
+
+        priority: this.selectedPriority,
+
+        editing: false
+
       });
 
-      this.newTask = '';
     }
+
+    this.saveTasks();
+
+    this.newTask = '';
+
+    this.selectedPriority = 'Medium';
+
   }
+
+  editTask(index: number) {
+
+    this.newTask = this.tasks[index].title;
+
+    this.selectedPriority =
+      this.tasks[index].priority;
+
+    this.editIndex = index;
+
+    this.isEditing = true;
+
+  }
+
+  completeTask(index: number) {
+
+  this.tasks[index].completed = true;
+
+  this.saveTasks();
+
+}
+
+
+
 
   deleteTodo(index: number) {
 
     this.tasks.splice(index, 1);
 
+    this.saveTasks();
+
   }
+
+  saveTasks() {
+
+    localStorage.setItem(
+
+      'tasks',
+
+      JSON.stringify(this.tasks)
+
+    );
+
+  }
+
 }
